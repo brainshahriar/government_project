3ca8f1d571b4ffc9608e76c6d62399657a776d46
brain <shahriarmehedi95@gmail.com>
2021-12-03 00:03:46 +0600

    done mastering

---
+++ resources/views/frontend/partials/left-sidebar.blade.php
@@ -0,0 +1,233 @@
+<div>
+    <div class="mayor-section">
+        <div class="content-header">
+            <h5>
+                <a href="#">
+                    সম্মানিত চেয়ারম্যান
+                </a>
+            </h5>
+        </div>
+        <a href="#">
+            <div class="mayor">
+                                    <div class="text-center">
+                    <img class="rounded img-fluid w-100 d-block"
+                        src="{{ asset('test.jpg') }}" alt="">
+                    <h5 class="mt-2 mb-0">নাম</h5>
+                    <div>
+                        পদবি
+                    </div>
+
+                </div>
+            </div>
+        </a>
+    </div>
+
+
+    <div class="councilor-section mt-3">
+        <div class="content-header">
+            <h5>
+                <a href="#">
+                    সম্মানিত চেয়ারম্যান
+                </a>
+            </h5>
+        </div>
+
+        <div class="councilor">
+            <div class="text-center">
+                <div class="form-row">
+                    <!-- Councilor By Ward -->
+                                                                    <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963735.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 1</h5>
+                            </div>
+                        </a>
+                    </div>
+                                         <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963756.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 2</h5>
+                            </div>
+                        </a>
+                    </div>
+                                         <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963778.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 3</h5>
+                            </div>
+                        </a>
+                    </div>
+                                         <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963784.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 4</h5>
+                            </div>
+                        </a>
+                    </div>
+                                         <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963791.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 5</h5>
+                            </div>
+                        </a>
+                    </div>
+                                         <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963797.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 6</h5>
+                            </div>
+                        </a>
+                    </div>
+                                         <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963804.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 7</h5>
+                            </div>
+                        </a>
+                    </div>
+                                         <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963810.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 8</h5>
+                            </div>
+                        </a>
+                    </div>
+                                         <div class="col-6">
+                        <a href="#">
+                            <div class="info">
+                                <img  class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963817.jpg" alt="">
+                                <h5 class="m-0 title">ওয়ার্ড 9</h5>
+                            </div>
+                        </a>
+                    </div>                                   
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- Councilor Section End  -->
+
+
+
+    <!-- Female Councilor Section Start  -->
+    <div class="councilor-section mt-3">
+        <div class="content-header">
+            <h5>
+                <a href="#">
+                    সংরক্ষিত আসনের চেয়ারম্যানগণ
+                </a>
+            </h5>
+        </div>
+                    <!--  Councilor By Ward -->
+        <div class="councilor">
+            <div class="text-center">
+                <div class="form-row">
+                    <!-- Councilor By Ward -->
+                                            <div class="col-4">
+                        <a href="#">
+                            <div class="info">
+                                <img class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963851.jpg" alt="">
+                            </div>
+                        </a>
+                    </div>
+
+                                           <div class="col-4">
+                        <a href="#">
+                            <div class="info">
+                                <img class="img-fluid w-100 d-block"
+                                    src="{{ asset('test.jpg') }}" alt="">
+                            </div>
+                        </a>
+                    </div>
+
+                                           <div class="col-4">
+                        <a href="#">
+                            <div class="info">
+                                <img class="img-fluid w-100 d-block"
+                                    src="upload/Councilor/1632963834.jpg" alt="">
+                            </div>
+                        </a>
+                    </div>
+
+                   
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- Female Councilor Section End -->
+
+    <!-- Important Links Start -->
+    <div class="application-link-section mt-3">
+        <div class="content-header">
+            <h5>
+                <a>
+                    গুরুত্বপূর্ণ আবেদনপত্র
+                </a>
+            </h5>
+        </div>
+
+        <div class="app-links ">
+            <div>
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action">ট্রেড লাইসেন্স অ্যাপ্লিকেশান ফরম</a>
+                    <a href="#" class="list-group-item list-group-item-action">উত্তরাধিকার সনদ ফরম</a>
+                    <a href="#" class="list-group-item list-group-item-action">হোল্ডিং নাম্বার ফরম</a>
+                    <a href="#" class="list-group-item list-group-item-action">জন্ম/মৃত্যু সনদ বাতিল/সংশোধনের আবেদনপত্র</a>
+                    <a href="#" class="list-group-item list-group-item-action">মৃত্যু সনদ ফরম</a>
+                    <a href="#" class="list-group-item list-group-item-action">মৃত্যু নিবন্ধন ফরম</a>
+                    <a href="#" class="list-group-item list-group-item-action">জন্ম নিবন্ধন ফরম</a>
+                  </div>
+            </div>
+        </div>
+    </div>
+    <!-- Important Links Start -->
+
+
+    <!-- Hotline Start -->
+    <div class="application-link-section mt-3">
+        <div class="content-header">
+            <h5>
+                <a>
+                    জরুরি হটলাইন
+                </a>
+            </h5>
+        </div>
+
+        <div class="app-links ">
+            <div>
+                <div>
+                    <img class="d-block w-100" src="{{ asset('test.jpg') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Hotline End -->
+
+
+
+
+
+
+
+</div>
---
+++ resources/views/frontend/partials/right-sidebar.blade.php
@@ -0,0 +1,126 @@
+<div class="col-md-3 order-3">
+    <div id="right-sidebar">
+   <div>
+   
+   <div class="add">
+       <img src="{{ asset('test.jpg') }}" alt="">
+   </div>
+   
+   
+   <!-- Notice Section Start -->
+   <div class="notice mb-3 mt-1">
+       <div class="content-header">
+           <h5>
+               <a href="#">
+                   আপডেট নোটিশ
+               </a>
+           </h5>
+       </div>
+   
+       <div class="content-body">
+           <div class="">
+               <marquee scrollamount='3' behavior="" direction="up">
+                   <a href="#" class="item">
+                   স্মার্ট জাতীয় পরিচয়পত্র বিতরন সময়সূচীসূচী
+                       <br><small>DD-MM-YYYY</small>
+                   </a>
+                   <a href="#" class="item">
+                      চাকুরীর আবেদন পত্র
+                       <br><small>DD-MM-YYYY</small>
+                   </a>
+                   <a href="#" class="item">
+                   নিয়োগ বিজ্ঞপ্তি
+                       <br><small>DD-MM-YYYY</small>
+                   </a>
+                   <a href="#" class="item">
+                 স্মার্ট সেনিটেশন সংক্রান্ত নোটিশ
+                       <br><small>DD-MM-YYYY</small>
+                   </a>
+                   <a href="#" class="item">
+                       সমস্ত নোটিশ দেখতে ক্লিক করুন
+                   </a>
+   
+               </marquee>
+   
+           </div>
+       </div>
+   
+   </div>
+   <!-- Notice Section Start -->
+   
+   
+   <!-- Links Start -->
+   <div class="links">
+       <div class="content-header">
+           <h5>
+               <a>
+                   গুরুত্বপূর্ণ লিঙ্কসমূহ
+               </a>
+           </h5>
+       </div>
+       <div class="content-body">
+           <div class="list-group">
+                                                        <a href="http://bris.lgd.gov.bd/pub/?pg=verify_br" target="_blank"
+                   class="list-group-item list-group-item-action">জন্ম নিবন্ধন যাচাই</a>
+                              <a href="https://mopa.gov.bd/" target="_blank"
+                   class="list-group-item list-group-item-action">জনপ্রশাসন মন্ত্রণালয়</a>
+                              <a href="https://bangladesh.gov.bd/index.php" target="_blank"
+                   class="list-group-item list-group-item-action">বাংলাদেশ জাতীয় তথ্য বাতায়ন</a>
+                              <a href="https://pmo.gov.bd/" target="_blank"
+                   class="list-group-item list-group-item-action">প্রধানমন্ত্রীর কার্যালয়</a>
+                              <a href="https://bangabhaban.gov.bd/" target="_blank"
+                   class="list-group-item list-group-item-action">রাষ্ট্রপতির কার্যালয়</a>
+                          </div>
+       </div>
+   </div>
+   <!-- Links End -->
+   
+   <!-- Digital Day Start -->
+   <div class="digital-day mt-3">
+       <div class="content-header">
+           <h5>
+               <a>
+                   ডিজিটাল দিবস
+               </a>
+           </h5>
+       </div>
+       <div>
+           <img class="d-block w-100" src="{{ asset('test.jpg') }}" alt="">
+       </div>
+   </div>
+   <!-- Digital Day End -->
+   
+   
+   <!-- Dengu Start -->
+   <div class="digital-day mt-3">
+       <div class="content-header">
+           <h5>
+               <a>
+                   করোনা ট্রেসার
+               </a>
+           </h5>
+       </div>
+       <div>
+           <img class="d-block w-100 border" src="{{ asset('test.jpg') }}" alt="">
+       </div>
+   </div>
+   <!-- Dengu End -->
+   
+   <!-- Dengu Start -->
+   <div class="digital-day mt-3">
+       <div class="content-header">
+           <h5>
+               <a>
+                   একদেশ
+               </a>
+           </h5>
+       </div>
+       <div>
+           <img class="d-block w-100 border" src="{{ asset('test.jpg') }}" alt="">
+       </div>
+   </div>
+   <!-- Dengu End -->
+   </div>
+   
+                   </div>
+               </div>
